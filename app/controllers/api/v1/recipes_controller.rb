83579028fa8a41504aa93ff0d961c489d939a2e9
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-10-15 14:25:56 +0900

    レシピコントローラーにユーザー認証と権限確認を追加

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,55 @@
 class Api::V1::RecipesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_recipe, only: [:show, :update, :destroy]
+  before_action :authorize_user!, only: [:show, :update, :destroy]
+
   def index
-    @recipes = Recipe.all
+    @recipes = current_user.recipes
     render json: @recipes
+  end
+
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
+
+    if @recipe.save
+      render json: @recipe, status: :created
+    else
+      render json: { errors: @recipe.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: @recipe
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      render json: @recipe
+    else
+      render json: { errors: @recipe.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Recipe not found" }, status: :not_found
+  end
+
+  def authorize_user!
+    unless @recipe.user_id == current_user.id
+      render json: { error: "You are not authorized to access this recipe" }, status: :forbidden
+    end
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :cooking_time, ingredients: [], instructions: [])
   end
 end
