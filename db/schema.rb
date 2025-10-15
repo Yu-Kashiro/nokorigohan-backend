3e9a615fc1509364d40f3e3afa3c08a955eeeb25
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-10-15 14:23:41 +0900

    recipesテーブルにuser_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_02_061443) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_15_050917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -27,6 +27,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_02_061443) do
     t.integer "cooking_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +42,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_02_061443) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "recipes", "users"
 end
